36d4e1e96f4a04c3d088a700ad16c9f184e20dc0
Philip Levy <philipdlevy@gmail.com>
2022-03-02 16:28:22 -0700

    commit tree 13299ea667f089c0ba9e0eb3a2969ba90a535e97

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+    document.addEventListener('DOMContentLoaded', () => {
+        let text = document.querySelector('#text')
+        text.textContent = "This is really cool!"
+    });
